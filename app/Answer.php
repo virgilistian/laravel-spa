b40f5d21efc986185003c3200ba9eb1c0b1ee84e
Virgi <virgi@torche.co.id>
2020-03-19 16:52:45 +0700

    Create answers schema

---
+++ app/Answer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Mail\Markdown;
+
+class Answer extends Model
+{
+    protected $fillable = ['question_id', 'user_id', 'body', 'votes_count'];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getBodyHtmlAttribute()
+    {
+        return Markdown::parse($this->body);
+    }
+}
