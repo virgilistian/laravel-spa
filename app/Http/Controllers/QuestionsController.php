22e090931727a674664c8756f31ee8f7a0e8598f
Virgi <virgi@torche.co.id>
2020-03-19 16:04:23 +0700

    Auth policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,12 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => 'index', 'show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -54,7 +60,7 @@ class QuestionsController extends Controller
     public function show(Question $question)
     {
         $question->increment('views');
-        // dd($question->body);
+        
         return view('questions.show', compact('question'));
     }
 
@@ -66,6 +72,8 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize('update', $question);
+
         return view('questions.edit', compact('question'));
     }
 
@@ -78,6 +86,8 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize('update', $question);
+
         $question->update($request->all());
 
         return redirect('/questions')->with('success', "Your question has been updated.");
@@ -91,6 +101,8 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize('delete', $question);
+
         $question->delete();
 
         return redirect('/questions')->with('success', "Your question has been deleted.");
