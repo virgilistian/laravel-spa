eb9a4431d968645b46938f17c2ecc03deb69d6c5
Virgi <virgi@torche.co.id>
2020-04-03 16:26:12 +0700

    Building load more answers feature

--- resources/js/components/Answers.vue
+++ resources/js/components/Answers.vue
@@ -14,6 +14,12 @@
                         :answer="answer"
                         :key="answer.id"
                     ></answer>
+
+                    <div class="text-center mt-3" v-if="nextUrl">
+                        <button @click.prevent="fetch(nextUrl)" class="btn btn-outline-secondary">
+                            Load more answers
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -23,7 +29,29 @@
 import Answer from "./Answer";
 
 export default {
-    props: ["answers", "count"],
+    props: ["question"],
+
+    data() {
+        return {
+            questionId: this.question.id,
+            count: this.question.answers_count,
+            answers: [],
+            nextUrl: null
+        };
+    },
+
+    created() {
+        this.fetch(`/questions/${this.questionId}/answers`);
+    },
+
+    methods: {
+        fetch(endpoint) {
+            axios.get(endpoint).then(({ data }) => {
+                this.answers.push(...data.data);
+                this.nextUrl = data.next_page_url;
+            });
+        }
+    },
 
     computed: {
         title() {
