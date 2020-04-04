c7fb9b9e22e60b60b59f3c9edc07452d835b5e75
Virgi <virgi@torche.co.id>
2020-04-04 22:28:50 +0700

    Make the tab preview functional

--- resources/js/components/MEditor.vue
+++ resources/js/components/MEditor.vue
@@ -3,10 +3,14 @@
         <div class="card-header">
             <ul class="nav nav-tabs card-header-tabs">
                 <li class="nav-item">
-                    <a class="nav-link active" data-toggle="tab" href="#write">Write</a>
+                    <a class="nav-link active" data-toggle="tab" href="#write"
+                        >Write</a
+                    >
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#preview">Preview</a>
+                    <a class="nav-link" data-toggle="tab" href="#preview"
+                        >Preview</a
+                    >
                 </li>
             </ul>
         </div>
@@ -14,13 +18,22 @@
             <div class="tab-pane active" id="write">
                 <slot></slot>
             </div>
-            <div class="tab-pane" id="preview">Preview...</div>
+            <div class="tab-pane" v-html="preview" id="preview"></div>
         </div>
     </div>
 </template>
 
 <script>
+import MarkdownIt from "markdown-it";
+const md = new MarkdownIt();
+
 export default {
-    props: ["body"]
+    props: ["body"],
+
+    computed: {
+        preview() {
+            return md.render(this.body);
+        }
+    }
 };
 </script>
